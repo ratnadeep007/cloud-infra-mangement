b9a0b4685825b9412eca3c3f45f46fa67f091697
Ratnadeep Bhattcharyya <ratnadeep.bhattacharyya@cura.tech>
2020-05-16 20:14:34 +0530

    rest api

--- aws-cloud-control-rest-api/handler.py
+++ aws-cloud-control-rest-api/handler.py
@@ -1,5 +1,20 @@
 import boto3
+from flask import Flask
+app = Flask(__name__)
 
-
+@app.route("/ec2")
 def rest(event, context):
-  pass
+  ec2 = boto3.resource('ec2', region_name='ap-south-1')
+  ec2_instances = []
+
+  filter = [
+    {
+      "Name": "tag:Type",
+      "Values": ["personal"]
+    }
+  ]
+  try:
+    for instance in ec2.instances.filter(Filters=filter):
+      ec2_instances.append(instance)
+    print(ec2_instances)
+    return "okay"
